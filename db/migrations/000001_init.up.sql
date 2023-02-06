48d12bf5b2399edafccb72723bbc27e77b381343
github dainoguchi <d.noguchi@buysell-technologies.com>
2023-02-06 11:10:54 +0900

    fix: ddlに対するレビュー反映(user_roles, priority, users.nameの長さ)

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE TABLE "users" (
                          "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
                          "auth0_id" varchar(40) UNIQUE NOT NULL,
-                         "name" varchar(20) NOT NULL,
+                         "name" varchar(40) NOT NULL,
                          "email" varchar(100) UNIQUE NOT NULL,
                          "picture" varchar(255),
                          "created_at" timestamptz NOT NULL DEFAULT 'now()',
@@ -24,7 +24,7 @@ CREATE TABLE "todos" (
 );
 
 CREATE TABLE "priorities" (
-                              "priority" varchar(20) PRIMARY KEY,
+                              "name" varchar(20) PRIMARY KEY,
                               "created_at" timestamptz NOT NULL DEFAULT 'now()',
                               "updated_at" timestamptz NOT NULL DEFAULT 'now()'
 );
@@ -48,10 +48,10 @@ CREATE TABLE "project_users" (
                                  "updated_at" timestamptz NOT NULL DEFAULT 'now()'
 );
 
-CREATE TABLE "user_roles" (
-                              "role" varchar(20) PRIMARY KEY,
-                              "created_at" timestamptz NOT NULL DEFAULT 'now()',
-                              "updated_at" timestamptz NOT NULL DEFAULT 'now()'
+CREATE TABLE "roles" (
+                         "role" varchar(20) PRIMARY KEY,
+                         "created_at" timestamptz NOT NULL DEFAULT 'now()',
+                         "updated_at" timestamptz NOT NULL DEFAULT 'now()'
 );
 
 CREATE UNIQUE INDEX ON "project_users" ("project_id", "user_id");
@@ -66,8 +66,8 @@ ALTER TABLE "todos" ADD FOREIGN KEY ("project_id") REFERENCES "projects" ("id") 
 
 ALTER TABLE "projects" ADD FOREIGN KEY ("owner_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
-ALTER TABLE "project_users" ADD FOREIGN KEY ("role") REFERENCES "user_roles" ("role") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "project_users" ADD FOREIGN KEY ("role") REFERENCES "roles" ("role") ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE "todos" ADD FOREIGN KEY ("priority") REFERENCES "priorities" ("priority") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "todos" ADD FOREIGN KEY ("priority") REFERENCES "priorities" ("name") ON DELETE SET NULL ON UPDATE CASCADE;
 
 COMMIT;
