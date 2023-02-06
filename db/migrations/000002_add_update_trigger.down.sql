a41795af895acdf115b743ae092958e57ccbeff3
github dainoguchi <d.noguchi@buysell-technologies.com>
2023-02-06 14:44:29 +0900

    feat: update trigger作成

---
+++ db/migrations/000002_add_update_trigger.down.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+drop trigger update_trigger on todos;
+drop trigger update_trigger on projects;
+drop trigger update_trigger on users;
+drop trigger update_trigger on project_users;
+drop trigger update_trigger on roles;
+drop trigger update_trigger on priorities;
+
+drop routine if exists set_updated_at();
+
+COMMIT;
---
+++ db/migrations/000002_add_update_trigger.up.sql
@@ -0,0 +1,36 @@
+CREATE FUNCTION set_updated_at() RETURNS trigger AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_trigger
+    BEFORE UPDATE
+    ON todos
+    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+CREATE TRIGGER update_trigger
+    AFTER UPDATE
+    ON projects
+    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+CREATE TRIGGER update_trigger
+    AFTER UPDATE
+    ON users
+    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+CREATE TRIGGER update_trigger
+    AFTER UPDATE
+    ON project_users
+    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+CREATE TRIGGER update_trigger
+    AFTER UPDATE
+    ON roles
+    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+CREATE TRIGGER update_trigger
+    AFTER UPDATE
+    ON priorities
+    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
