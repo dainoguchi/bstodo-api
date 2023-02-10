15165ebb3860a1645e4a6c80d8f880ef7e196b38
github dainoguchi <d.noguchi@buysell-technologies.com>
2023-02-10 11:58:26 +0900

    feat: handlerをrouteに追加

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"github.com/dainoguchi/bstodo-api/config"
 	"github.com/dainoguchi/bstodo-api/infra/postgres"
+	"github.com/dainoguchi/bstodo-api/restapi/handler"
+	"github.com/dainoguchi/bstodo-api/usecase"
 	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
@@ -18,23 +21,27 @@ func main() {
 
 func run(ctx context.Context) error {
 	cfg, _ := config.New()
-	router := NewRouter()
 
-	// 今はdb connection生成されてるか確認するのみ
-	_, err := postgres.New()
+	db, err := postgres.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	router := NewRouter(db)
+
 	return http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), router)
 }
 
-func NewRouter() http.Handler {
+func NewRouter(db *sql.DB) http.Handler {
 	router := chi.NewRouter()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello World!")
 	})
+
+	// 試しにuser１件取得するのみ
+	uh := handler.NewUserHandler(usecase.NewUserUsecase(db))
+	router.HandleFunc("/user", uh.GetByID)
 
 	return router
 }
