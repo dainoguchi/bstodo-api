d6f99f5ed8ddffb58a014d3f54e235a03adf1821
github dainoguchi <d.noguchi@buysell-technologies.com>
2023-02-10 11:59:17 +0900

    feat: handlerから呼び出すusecaseの作成(処理は空)

---
+++ usecase/user.go
@@ -0,0 +1,24 @@
+package usecase
+
+import (
+	"context"
+	"database/sql"
+	"github.com/dainoguchi/bstodo-api/entity"
+)
+
+type UserUsecase interface {
+	GetByID(context.Context, string) (*entity.User, error)
+}
+
+func NewUserUsecase(db *sql.DB) UserUsecase {
+	return &userUsecase{db: db}
+}
+
+type userUsecase struct {
+	db *sql.DB
+}
+
+// 実装はまだ
+func (u userUsecase) GetByID(ctx context.Context, id string) (*entity.User, error) {
+	return &entity.User{ID: id, Name: "テスト君"}, nil
+}
