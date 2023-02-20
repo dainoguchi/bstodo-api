d33f3fd3231073a4ca6f9d00574069396ea681a4
github dainoguchi <d.noguchi@buysell-technologies.com>
2023-02-20 16:38:00 +0900

    feat: 公式ドキュメント通り実装

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require github.com/caarlos0/env/v7 v7.0.0
 
 require (
+	github.com/auth0/go-jwt-middleware/v2 v2.1.0
 	github.com/go-playground/validator/v10 v10.11.2
 	github.com/google/uuid v1.3.0
 	github.com/labstack/echo/v4 v4.10.0
@@ -14,14 +15,19 @@ require (
 require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/satishbabariya/go-echo-auth0-middleware v0.0.0-20220408142446-3d452cd053da // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/time v0.2.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
---
+++ internal/restapi/middleware/auth.go
@@ -0,0 +1,80 @@
+package middleware
+
+import (
+	"context"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
+	"github.com/auth0/go-jwt-middleware/v2/jwks"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+type AuthMiddleware interface {
+	EnsureValidToken(next http.Handler) http.Handler
+}
+
+type authMiddleware struct{}
+
+func NewAuthMiddleware() AuthMiddleware {
+	return &authMiddleware{}
+}
+
+// コピペ
+
+// CustomClaims contains custom data we want from the token.
+type CustomClaims struct {
+	Scope string `json:"scope"`
+}
+
+// Validate does nothing for this example, but we need
+// it to satisfy validator.CustomClaims interface.
+func (c CustomClaims) Validate(ctx context.Context) error {
+	return nil
+}
+
+// EnsureValidToken is a middleware that will check the validity of our JWT.
+func (a *authMiddleware) EnsureValidToken(next http.Handler) http.Handler {
+	issuerURL, err := url.Parse("https://" + os.Getenv("AUTH0_DOMAIN") + "/")
+	if err != nil {
+		log.Fatalf("Failed to parse the issuer url: %v", err)
+	}
+
+	provider := jwks.NewCachingProvider(issuerURL, 5*time.Minute)
+
+	// parserの本体。jwtValidator.ValidateTokenにtoken渡すとauth0の公開鍵拾ってきて検証するっぽい
+	jwtValidator, err := validator.New(
+		provider.KeyFunc,
+		validator.RS256,
+		issuerURL.String(),
+		[]string{os.Getenv("AUTH0_AUDIENCE")},
+		validator.WithCustomClaims(
+			func() validator.CustomClaims {
+				return &CustomClaims{}
+			},
+		),
+		validator.WithAllowedClockSkew(time.Minute),
+	)
+	if err != nil {
+		log.Fatalf("Failed to set up the jwt validator")
+	}
+
+	errorHandler := func(w http.ResponseWriter, r *http.Request, err error) {
+		log.Printf("Encountered error while validating JWT: %v", err)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"message":"Failed to validate JWT."}`))
+	}
+
+	// token取得してValidateToken関数を実行してるみたい
+	// error発生時第２引数のエラーハンドラー呼び出すのか
+	middleware := jwtmiddleware.New(
+		jwtValidator.ValidateToken,
+		jwtmiddleware.WithErrorHandler(errorHandler),
+	)
+
+	return middleware.CheckJWT(next)
+}
