fbfd3b5e167c8cb9a04c574ea859f20c19821f4a
github dainoguchi <d.noguchi@buysell-technologies.com>
2023-02-20 16:38:42 +0900

    feat: jwtmiddlewareを排除し、echo用handlerに書き換え

--- internal/restapi/middleware/auth.go
+++ internal/restapi/middleware/auth.go
@@ -2,18 +2,20 @@ package middleware
 
 import (
 	"context"
-	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
+	"fmt"
 	"github.com/auth0/go-jwt-middleware/v2/jwks"
 	"github.com/auth0/go-jwt-middleware/v2/validator"
+	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 )
 
 type AuthMiddleware interface {
-	EnsureValidToken(next http.Handler) http.Handler
+	EnsureValidToken(next echo.HandlerFunc) echo.HandlerFunc
 }
 
 type authMiddleware struct{}
@@ -21,8 +23,6 @@ type authMiddleware struct{}
 func NewAuthMiddleware() AuthMiddleware {
 	return &authMiddleware{}
 }
-
-// コピペ
 
 // CustomClaims contains custom data we want from the token.
 type CustomClaims struct {
@@ -36,7 +36,7 @@ func (c CustomClaims) Validate(ctx context.Context) error {
 }
 
 // EnsureValidToken is a middleware that will check the validity of our JWT.
-func (a *authMiddleware) EnsureValidToken(next http.Handler) http.Handler {
+func (a *authMiddleware) EnsureValidToken(next echo.HandlerFunc) echo.HandlerFunc {
 	issuerURL, err := url.Parse("https://" + os.Getenv("AUTH0_DOMAIN") + "/")
 	if err != nil {
 		log.Fatalf("Failed to parse the issuer url: %v", err)
@@ -61,20 +61,35 @@ func (a *authMiddleware) EnsureValidToken(next http.Handler) http.Handler {
 		log.Fatalf("Failed to set up the jwt validator")
 	}
 
-	errorHandler := func(w http.ResponseWriter, r *http.Request, err error) {
-		log.Printf("Encountered error while validating JWT: %v", err)
+	// echoに書き換え
+	return func(c echo.Context) error {
+		// 普通にechoでjwtのトークン抜き出すコード
+		authorization := c.Request().Header.Get("Authorization")
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte(`{"message":"Failed to validate JWT."}`))
+		authHeaders := strings.Split(authorization, " ")
+		if len(authHeaders) != 2 {
+			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Invalid authorization header, should format `Bearer code`, but got %s\n", authHeaders))
+		}
+
+		ctx := c.Request().Context()
+
+		// 取得したtokenをvalidatorに渡す
+		// 本当は外部から注入したい
+		token, err := jwtValidator.ValidateToken(ctx, authHeaders[1])
+		if err != nil {
+			return echo.NewHTTPError(http.StatusUnauthorized, fmt.Sprintf("Invalid jwt token. %s", err.Error()))
+		}
+
+		log.Printf("parsed token: %s\n", token)
+
+		// with value の keyは空構造体が良いらしい
+		ctx = context.WithValue(ctx, TokenContextKey{}, token)
+		c.SetRequest(c.Request().WithContext(ctx))
+
+		log.Println("context.value: ", c.Request().Context().Value(TokenContextKey{}))
+
+		return next(c)
 	}
-
-	// token取得してValidateToken関数を実行してるみたい
-	// error発生時第２引数のエラーハンドラー呼び出すのか
-	middleware := jwtmiddleware.New(
-		jwtValidator.ValidateToken,
-		jwtmiddleware.WithErrorHandler(errorHandler),
-	)
-
-	return middleware.CheckJWT(next)
 }
+
+type TokenContextKey struct{}
