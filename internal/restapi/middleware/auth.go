438fc43bfc69c148be2c74c4dee21f8a427b57ac
github dainoguchi <d.noguchi@buysell-technologies.com>
2023-02-20 19:51:02 +0900

    fix: authorization headerのフォーマットエラーも401を返す

--- internal/restapi/middleware/auth.go
+++ internal/restapi/middleware/auth.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/dainoguchi/bstodo-api/internal/infra/auth0"
 	"github.com/labstack/echo/v4"
-	"log"
 	"net/http"
 	"strings"
 )
@@ -22,38 +21,31 @@ func NewAuthMiddleware(jwtValidator auth0.JwtValidator) AuthMiddleware {
 	return &authMiddleware{jwtValidator: jwtValidator}
 }
 
-// EnsureValidToken is a middleware that will check the validity of our JWT.
 func (a *authMiddleware) EnsureValidToken(next echo.HandlerFunc) echo.HandlerFunc {
 
-	// echoに書き換え
 	return func(c echo.Context) error {
 		// 普通にechoでjwtのトークン抜き出すコード
 		authorization := c.Request().Header.Get("Authorization")
 
 		authHeaders := strings.Split(authorization, " ")
 		if len(authHeaders) != 2 {
-			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Invalid authorization header, should format `Bearer code`, but got %s\n", authHeaders))
+			return echo.NewHTTPError(http.StatusUnauthorized, fmt.Sprintf("Invalid authorization header, should format `Bearer code`, but got %s\n", authHeaders))
 		}
 
 		ctx := c.Request().Context()
 
-		// 取得したtokenをvalidatorに渡す
-		// 本当は外部から注入したい
+		// jwtトークンをパースし、auth0.JwtToken型の変数を返す
 		token, err := a.jwtValidator.ValidateToken(ctx, authHeaders[1])
 		if err != nil {
 			return echo.NewHTTPError(http.StatusUnauthorized, fmt.Sprintf("Invalid jwt token. %s", err.Error()))
 		}
 
-		log.Printf("parsed token: %s\n", token)
-
-		// with value の keyは空構造体が良いらしい
 		ctx = context.WithValue(ctx, TokenContextKey{}, token)
 		c.SetRequest(c.Request().WithContext(ctx))
-
-		log.Println("context.value: ", c.Request().Context().Value(TokenContextKey{}))
 
 		return next(c)
 	}
 }
 
+// with value の keyは空構造体が良いらしい
 type TokenContextKey struct{}
