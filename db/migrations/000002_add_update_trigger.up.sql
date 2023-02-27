12003c4359606801916cea80b49f50d4f5961c76
github dainoguchi <d.noguchi@buysell-technologies.com>
2023-02-27 13:48:18 +0900

    fix: 合わせてシナリオをシンプルにする

--- db/migrations/000002_add_update_trigger.up.sql
+++ db/migrations/000002_add_update_trigger.up.sql
@@ -1,33 +1,13 @@
 CREATE FUNCTION set_updated_at() RETURNS trigger AS $$
 BEGIN
     NEW.updated_at = now();
-    RETURN NEW;
+RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_trigger
     BEFORE UPDATE
     ON todos
-    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
-
-CREATE TRIGGER update_trigger
-    AFTER UPDATE
-    ON projects
-    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
-
-CREATE TRIGGER update_trigger
-    AFTER UPDATE
-    ON users
-    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
-
-CREATE TRIGGER update_trigger
-    AFTER UPDATE
-    ON project_users
-    FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
-
-CREATE TRIGGER update_trigger
-    AFTER UPDATE
-    ON roles
     FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
 
 CREATE TRIGGER update_trigger
