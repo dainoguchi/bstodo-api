0d37e477b213b25d46eaa79d48b26bf6ddc2f38d
github dainoguchi <d.noguchi@buysell-technologies.com>
2023-03-09 07:43:14 +0900

    feat: テスト用にpgx.Txとpgx.Connの抽象化

--- internal/infra/postgres/postgres.go
+++ internal/infra/postgres/postgres.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 	"fmt"
+	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 )
 
@@ -19,3 +20,15 @@ func New(dsnConfig DSNConfig) (*pgx.Conn, error) {
 
 	return conn, nil
 }
+
+type PgxWrapper interface {
+	Begin(ctx context.Context) (pgx.Tx, error)
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, optionsAndArgs ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, optionsAndArgs ...interface{}) pgx.Row
+}
+
+var (
+	_ PgxWrapper = (*pgx.Conn)(nil)
+	_ PgxWrapper = (pgx.Tx)(nil)
+)
---
+++ internal/restapi/handler/todo.go
@@ -0,0 +1,70 @@
+package handler
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"time"
+)
+
+type TodoHandler struct {
+	// usecaseのinterface。コンストラクタで実態を渡す
+	uc usecase.TodoUsecase
+}
+
+func NewTodoHandler(uc usecase.TodoUsecase) *TodoHandler {
+	return &TodoHandler{uc: uc}
+}
+
+func (h *TodoHandler) Create(c echo.Context) error {
+	var req struct {
+		Title       string     `json:"title"`
+		Priority    string     `json:"priority"`
+		Description *string    `json:"description"`
+		DueDate     *time.Time `json:"due_date"`
+	}
+
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	ctx := c.Request().Context()
+	auth0ID := "auth0ID"
+
+	todo, err := h.uc.CreateTodo(ctx, &input.CreateTodoInput{
+		Title:       req.Title,
+		Description: req.Description,
+		Priority:    req.Priority,
+		DueDate:     req.DueDate,
+		Auth0ID:     auth0ID,
+	})
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	// response 詰め替え
+	// sqlcのresponseにomitemptyをつけるオプションが存在しなかったのです,,,
+	res := struct {
+		ID          uuid.UUID  `json:"id"`
+		Title       string     `json:"title"`
+		Description *string    `json:"description,omitempty"`
+		Done        bool       `json:"done"`
+		Priority    string     `json:"priority"`
+		DueDate     *time.Time `json:"due_date,omitempty"`
+		Auth0ID     string     `json:"auth0_id"`
+		CreatedAt   time.Time  `json:"created_at"`
+		UpdatedAt   time.Time  `json:"updated_at"`
+	}{
+		ID:          todo.ID,
+		Title:       todo.Title,
+		Description: todo.Description,
+		Done:        todo.Done,
+		Priority:    todo.Priority,
+		DueDate:     todo.DueDate,
+		Auth0ID:     todo.Auth0ID,
+		CreatedAt:   todo.CreatedAt,
+		UpdatedAt:   todo.UpdatedAt,
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
